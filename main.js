126a82939362aba9dd9ffdf2cefa1be806498049
funam <funamario071@gmail.com>
2022-04-24 00:35:56 +0900

    ポートフォリオをすすめた

---
+++ main.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("button");
+const image = document.getElementById("image");
+button.onclick = function () {
+  image.src = "https://www.shonenjump.com/j/img/_news/181105op_1.jpg";
+};
